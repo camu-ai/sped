44895e07469327ed194f537b72e39024775cfa24
Phelipe Alves de Souza <sougyn@gmail.com>
2025-07-10 23:21:59 -0300

    feat: add sped-nfe (#1)
    * feat: add sped-nfe
    
    * chore: update README with badge and comment out build-example workflow
    
    * fix: add newline at end of README.md
    
    * chore: update repository URL format in package.json
    
    * chore: update compressed size action for sped-nfe package and add exclusion for map files
    
    * chore: specify build and install scripts for compressed size action in sped-nfe package
    
    * chore: comment out build and install scripts for compressed size action in sped-nfe package
    
    * chore: update build script for compressed size action in sped-nfe package to use frozen lockfile
    
    * chore: restore and configure parameters for compressed size action in sped-nfe package

---
+++ packages/sped-nfe/src/nfe.ts
@@ -0,0 +1,5 @@
+export class SpedNFe {
+  constructor(private configs: { nfe: { chNFe: string; tpNFe: string } }) {
+    console.log(this.configs)
+  }
+}
---
+++ packages/sped-nfe/src/types.ts
@@ -0,0 +1,10 @@
+export interface NFeCapture {
+  nfe: {
+    chNFe: string
+    tpNFe: string
+    dhEmi: string
+    tpEmis: string
+    tpAmb: string
+    tpNF: string
+  }
+}
