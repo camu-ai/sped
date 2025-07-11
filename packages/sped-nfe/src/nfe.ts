abaf3306995711ff3365ac6b0a03ed28f234d694
Phelipe Alves de Souza <sougyn@gmail.com>
2025-07-11 00:38:55 -0300

    feat: enhance NFe logging and add capture method in SpedNFe class (#6)

--- packages/sped-nfe/src/nfe.ts
+++ packages/sped-nfe/src/nfe.ts
@@ -1,6 +1,11 @@
 export class SpedNFe {
   constructor(private configs: { nfe: { chNFe: string; tpNFe: string } }) {
     console.log(`NFe document capture: ${this.configs.nfe.chNFe}`)
-    console.log(this.configs)
+    console.log(`NFe document capture: ${this.configs.nfe.tpNFe}`)
+  }
+
+  capture(): string {
+    console.log(`NFe document capture: ${this.configs.nfe.chNFe}`)
+    return this.configs.nfe.chNFe
   }
 }
