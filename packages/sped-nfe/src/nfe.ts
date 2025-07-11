c8fb35faae3f10567af832ee420c3fa547b815a4
Phelipe Alves de Souza <sougyn@gmail.com>
2025-07-11 00:31:00 -0300

    feat: log NFe document capture in SpedNFe constructor

--- packages/sped-nfe/src/nfe.ts
+++ packages/sped-nfe/src/nfe.ts
@@ -1,5 +1,6 @@
 export class SpedNFe {
   constructor(private configs: { nfe: { chNFe: string; tpNFe: string } }) {
+    console.log(`NFe document capture: ${this.configs.nfe.chNFe}`)
     console.log(this.configs)
   }
 }
